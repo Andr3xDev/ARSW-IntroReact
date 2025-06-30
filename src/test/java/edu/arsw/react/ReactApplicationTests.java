d701cde8d671fb7eefbda5f4092ffbb4b6e05b37
Andr3xDev <achavarropp@gmail.com>
2025-06-30 07:39:48 -0500

    add: initial proyect

---
+++ src/test/java/edu/arsw/react/ReactApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.arsw.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
